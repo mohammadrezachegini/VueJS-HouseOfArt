ba66ce67299fdcca7c19fcfb72aa071cfca15080
Reza <mohammadreza.chegini01@gmail.com>
2023-03-22 13:55:21 -0700

    create route for customer  login

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+
+import CustomerLogin from "../components/CustomerLogin.vue";
+import PhotographerLogin from "../components/PhotographerLogin.vue";
+import CustomerRegister from "../components/CustomerRegister.vue";
+import PhotographerRegister from "../components/PhotographerRegister.vue";
+
+
+
+
+const routes = [
+  {
+    path: "/",
+    alias: "/customer-login",
+    name: "CustomerLogin",
+    component: CustomerLogin
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+export default router;
